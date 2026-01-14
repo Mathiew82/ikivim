4bfc30c9c190a1645e23fca3b7d2afa449b8baf4
Mathiew82 <amateo1982@yahoo.es>
2026-01-14 14:48:55 +0100

    Add conform plugin to format code with prettier

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          json = { "prettier" },
+          css = { "prettier" },
+          html = { "prettier" },
+        },
+
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
+    end,
+  },
+}
+
