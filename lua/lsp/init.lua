62eca0e0841d4e91103455be3ac18901d0bb3e8d
Mathiew82 <amateo1982@yahoo.es>
2026-01-19 02:19:22 +0100

    Update diagnostic keymaps to use new jump API

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -29,12 +29,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "[d", function()
+      vim.diagnostic.jump({ count = -1 })
+    end, opts)
+    vim.keymap.set("n", "]d", function()
+      vim.diagnostic.jump({ count = 1 })
+    end, opts)
     vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
   end,
 })
